80b97186848dcd321c2f0db68ab9ad76f82604e7
BigKing <641648019@qq.com>
2018-06-27 13:48:25 +0800

    hh

---
+++ XBZKit/XBZKit/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  XBZKit
+//
+//  Created by BigKing on 2018/6/27.
+//  Copyright © 2018年 BigKing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    NSLog(@"");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
